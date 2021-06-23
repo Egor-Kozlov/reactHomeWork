93d2f1262e8a147e78353141b91b8a01ea196de6
Egor-Kozlov <60579020+Egor-Kozlov@users.noreply.github.com>
2021-06-23 16:13:15 +0300

    Revert "feat: add tasks 4.1, 4.2, 4.3"

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import {ReactComponent as HelpIcon} from '../../img/icons/icon_help.svg'
-class Card extends React.Component {
-    
-    render () {
+
+
+function Card(props) {
     return (
         <section className='card'>
             <div className="card__header-container">
                 <h3 className='card__header'>
-                    Task {this.props.taskNumber}
+                    Task {props.taskNumber}
                 </h3>
-                <div className="icon-help" data-tip data-for={this.props.condition}>
-                    <HelpIcon />
-                </div> 
+                <div className="icon-help"><HelpIcon /></div>
             </div>
-            <div className='card__solution'>{this.props.children}</div>
+            <p className='card__description'>{props.condition}</p>
+            <div className='card__solution'>{props.children}</div>
         </section>
     )
-    }
 }
 
 export default Card
--- src/components/task-3.1/Text.jsx
+++ src/components/task-3.1/Text.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Text(props) {
     return (
-            <p className = 'text' style = {{color: props.textColor, 
-                fontSize: props.textSize + 'px', 
-                textDecoration: props.textDecoration ? 'underline' : 'none'}}>{props.text}
-            </p>  
+        <p className = 'text' style = {{color: props.textColor, 
+            fontSize: props.textSize + 'px', 
+            textDecoration: props.textDecoration ? 'underline' : 'none'}}>{props.text}
+        </p>
     )
 }
 
