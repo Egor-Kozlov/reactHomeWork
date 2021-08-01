734a4996678422a65f136269c489636e09015b26
Egor-Kozlov <kozlov1998.2010@gmail.com>
2021-08-01 16:46:47 +0300

    feat: add task H.9.3

--- src/components/task-H.9.3/TimePicker.jsx
+++ src/components/task-H.9.3/TimePicker.jsx
@@ -1,21 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-class TimePicker extends React.Component {
+function TimePicker() {
 
+    const [hours, setHours] = useState(0)
+    const [minutes, setMinutes] = useState(0)
 
-    render() {
+    useEffect(() => {
+        if (hours > 23) {
+            setHours(0)
+        }
+        if (hours < 0) {
+            setHours(23)
+        }
+        if (minutes > 59) {
+            setMinutes(0)
+            setHours(hours + 1)
+        }
+        if (minutes < 0) {
+            setHours(hours - 1)
+            setMinutes(59)
+        }
+    }, [hours, minutes])
 
-        console.log('render');
-
-        return (
-
-            console.log('return'),
-
-            <div>
-                hello
+    return (
+        <div className='time-picker-container'>
+            <div className="counter">
+                <span className='counter__item'>{hours < 10 ? `0${hours}` : hours}</span> : 
+                <span className='counter__item'>{minutes < 10 ? `0${minutes}` : minutes}</span>
             </div>
-        )
-    }
+            <div className="buttons-container">
+                <div className="buttons-hours">
+                    <button onClick = {() => setHours(hours - 1)}>-</button>
+                    <button onClick = {() => setHours(hours + 1)}>+</button>
+                </div>
+                <div className="buttons-minutes">
+                    <button onClick = {() => setMinutes(minutes - 1)}>-</button>
+                    <button onClick = {() => setMinutes(minutes + 1)}>+</button>
+                </div>
+            </div> 
+        </div>
+    )
 }
 
 export default TimePicker
